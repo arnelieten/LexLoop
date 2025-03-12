9ecdc2fb0f06e177fed8e0bee36e51bff681e08e
Arne Lieten <arne.lieten@hotmail.com>
2025-03-12 08:39:46 +0100

    Modified process file to insert the word pairs into a database instead of exporting as an excel file. Created the dashboard where the french-english word pairs will appear filtered for each user so that they only see their own word pairs

--- lexloop/process.py
+++ lexloop/process.py
@@ -77,4 +77,10 @@ def process_file(filename):
 
     db.commit()
 
-    return render_template('auth/register.html')
+    words = db.execute('''
+        SELECT french_word, english_word
+        FROM dashboard dash JOIN dictionary dict ON dash.dictionary_id = dict.id
+        WHERE dash.user_id = ?
+        ''', (user_id,)).fetchall()
+
+    return render_template('dashboard/dashboard.html', words=words)
