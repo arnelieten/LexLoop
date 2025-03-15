3470fc95532551bec182097fe4f4be0915d6774e
Arne Lieten <arne.lieten@hotmail.com>
2025-03-15 18:40:17 +0100

    Saved the configurations in a seperate config.py file that will not be synced with GitHub

--- lexloop/__init__.py
+++ lexloop/__init__.py
@@ -4,15 +4,10 @@ from flask import Flask,render_template
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev', #set to api or randdom key when deploying & set config.py file
-        DATABASE=os.path.join(app.instance_path, 'lexloop.sqlite'),
-        UPLOADS=os.path.join(app.instance_path, 'uploads'),
-        MAX_CONTENT_LENGTH = 100 * 1024 * 1024
-    )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py')
+        print(app.config)
     else:
         app.config.from_mapping(test_config)
 
